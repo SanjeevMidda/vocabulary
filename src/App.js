506afb477ed45afba3ec04039aaba7d3ee0a8af4
Sanjeev Midda <110365686+SanjeevMidda@users.noreply.github.com>
2024-10-20 20:34:30 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import Card from './components/Card';
+import Colors from './components/Colors';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+<div className="mainContainer">
+<header>
+        <div className="circle"></div>
+        <h1>vocabulary</h1>
       </header>
+
+      <main>
+        <Colors />
+      </main>
+</div>
+     
     </div>
   );
 }
+
+export default App;
+
 
 export default App;
