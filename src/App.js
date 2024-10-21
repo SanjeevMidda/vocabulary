a7a2016cbfe669ffbf776f904a265923d7e79114
Sanjeev Midda <110365686+SanjeevMidda@users.noreply.github.com>
2024-10-21 10:54:40 +0100

    Update App.js
    Updated with emotion's component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,70 @@
-import './index.css';
-import Card from './components/Card';
-import Colors from './components/Colors';
+import "./index.css";
+import Card from "./components/Card";
+import Colors from "./components/Colors";
+import Actions from "./components/Actions";
+import Emotions from "./components/Emotions";
+
+import { useState } from "react";
 
 function App() {
+  const [options, setOptions] = useState(false);
+  const [selected, setSelected] = useState("vocabulary");
+
+  let optionsStyle = {
+    // transform: `translateY(${options ? "-380px" : "-500px"})`,
+    transition: "all 1s",
+    top: `${options ? "-25px" : "-150px"}`,
+  };
+
+  let textToDisplay = "";
+
+  if (selected === "colors") {
+    textToDisplay = <Colors />;
+  } else if (selected === "actions") {
+    textToDisplay = <Actions />;
+  } else if (selected === "emotions") {
+    textToDisplay = <Emotions />;
+  }
+
   return (
     <div className="App">
+      <div className="mainContainer">
+        <header>
+          <div className="circle" onClick={() => setOptions(!options)}></div>
+          <h1>{selected}</h1>
+        </header>
 
-<div className="mainContainer">
-<header>
-        <div className="circle"></div>
-        <h1>vocabulary</h1>
-      </header>
+        <main>{textToDisplay}</main>
+      </div>
 
-      <main>
-        <Colors />
-      </main>
-</div>
-     
+      <div className="options" style={optionsStyle}>
+        <h2
+          onClick={() => {
+            setOptions(false);
+            setSelected("colors");
+          }}
+        >
+          colors
+        </h2>
+        <h2
+          onClick={() => {
+            setOptions(false);
+            setSelected("actions");
+          }}
+        >
+          actions
+        </h2>
+        <h2
+          onClick={() => {
+            setOptions(false);
+            setSelected("emotions");
+          }}
+        >
+          emotions
+        </h2>
+      </div>
     </div>
   );
 }
-
-export default App;
-
 
 export default App;
